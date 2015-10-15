c94389c2ae65965ee46d4ba55e7780c53bf565ab
caoyaxin <caobosen@hotmail.com>
2015-10-15 18:38:31 -0400

    first java class for git demo project

---
+++ gitDemo/src/gitDemo/HelloDemo.java
@@ -0,0 +1,11 @@
+package gitDemo;
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! This is a demo that How to use Git hub!");
+
+	}
+
+}
