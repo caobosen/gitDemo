acd7069ad8940d3dbb230c38c06cbfa3ea9bafd1
caoyaxin <caobosen@hotmail.com>
2015-10-15 18:48:28 -0400

    add message where?

--- gitDemo/src/gitDemo/HelloDemo.java
+++ gitDemo/src/gitDemo/HelloDemo.java
@@ -5,6 +5,7 @@ public class HelloDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! This is a demo that How to use Git hub!");
+		System.out.println("Where are you my firend!");
 
 	}
 
